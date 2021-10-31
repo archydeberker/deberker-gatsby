1ab4a3fc8a94ea203f86ffc0a39623c70f6e7722
Archy de Berker <archy.deberker@gmail.com>
2021-10-31 22:10:10 +0000

    Update links in About

--- src/pages/about.js
+++ src/pages/about.js
@@ -14,32 +14,31 @@ const AboutPage = () => {
         <img src={banner} className="bannerImage"></img>
         <p>
           I lead data science at
-          <Link href="https://www.carbonchain.io"> CarbonChain </Link>, a
+          <Link href="https://www.carbonchain.io"> CarbonChain</Link>, a
           London-based startup building carbon footprinting software for
-          carbon-intense supply chains.
+          ultra-high-emitting supply chains.
         </p>
         <p>
-          I like to build products which use data and machine learning to
-          deliver magical experiences. I like to write code myself, and help
-          figure out what code teams should write. In my current role I do a bit
-          of both.
+          I build products which use data and machine learning to deliver
+          magical experiences. I like to write code myself, and help figure out
+          what code teams should write. In my current role I do a bit of both.
         </p>
         <p>
           I previously worked on global emissions monitoring as part of Vice
           President Al Gore's
-          <Link>Climate TRACE </Link>project, and in applied ML research and
-          product management at
-          <Link>Element AI in Montreal </Link>. I've also done stints as a
-          consultant for interesting climate tech companies like Transition
-          Zero, Habitat, and Origami Energy.{" "}
+          <Link href="https://www.climatetrace.org"> Climate TRACE </Link>
+          project, and in applied ML research and product management at
+          <Link href="https://www.elementai.com"> Element AI </Link>. I've also
+          done stints as a consultant for interesting climate tech companies
+          like{" "}
+          <Link href="https://www.transitionzero.org">Transition Zero</Link>,{" "}
+          <Link href="https://www.habitat.energy">Habitat</Link>, and{" "}
+          <Link href="https://www.origamienergy.com"> Origami Energy </Link>.
         </p>
         <p>
           I have a PhD in Neuroscience from University College London, and an
           undergrad in Natural Sciences from Pembroke College, Cambridge.
         </p>
-      </div>
-      <div>
-        <h2> Contact </h2>
       </div>
     </Layout>
   )
